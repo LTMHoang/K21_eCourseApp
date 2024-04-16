d538f7feb5d0167fa87955ab59e43533a6faea5e
LTMHoang <letruongminhhoang2003@gmail.com>
2024-04-16 16:28:55 +0700

    thuc hanh 16 4 2024

---
+++ eCourseApp_Demo/components/Course/Course.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, ScrollView, Image } from "react-native"
+import { ActivityIndicator, Chip, List } from "react-native-paper";
+import APIs, { endpoints } from "../../configs/APIs";
+import MyStyle from "../../styles/MyStyle"
+
+const Course = () => {
+    const [categories, setCategories] = React.useState(null);
+    const [courses, setCourses] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
+
+    const loadCates = async () => {
+        try {
+            let res = await APIs.get(endpoints['categories']);
+            setCategories(res.data);
+        } catch (ex) {
+            console.error(ex)
+        }
+    }
+
+    const loadCourses = async () => {
+        try {
+            setLoading(true);
+            let res = await APIs.get(endpoints['courses']);
+            setCourses(res.data.results);
+        } catch (ex) {
+            console.error(ex)
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    React.useEffect(() => {
+        loadCates();
+    }, []);
+
+    React.useEffect(() => {
+        loadCourses();
+    }, []);
+
+    return (
+        <View style={MyStyle.container}>
+            <Text style={MyStyle.subject}>DANH MỤC KHÓA HỌC</Text>
+            <View style={MyStyle.row}>
+            {categories===null?<ActivityIndicator />:<>
+                {categories.map(c => <Chip style={MyStyle.margin} key={c.id} icon="shape">{c.name}</Chip>)}
+            </>}
+            </View>
+
+            <ScrollView>
+                {courses.map(c => <List.Item key={c.id} title={c.subject} description={c.created_date} left={() => <Image style={MyStyle.avatar} source={{uri: c.image}}/>}/>)}
+                {loading && <ActivityIndicator/>}
+            </ScrollView>
+        </View>
+    )
+}
+
+export default Course;
--- ecourseltmh/courses/admin.py
+++ ecourseltmh/courses/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from courses.models import *
 from django.utils.html import mark_safe
 from django import forms
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.urls import path
+from django.template.response import TemplateResponse
+
+
+class MyCourseAdminSite(admin.AdminSite):
+    site_header = 'eCourseOnline'
+
+    def get_urls(self):
+        return [path('course-stats/', self.stats_view)] + super().get_urls()
+
+    def stats_view(self, request):
+        course_stats = Category.objects.annotate(c=Count('course__id')).values('id', 'name', 'c')
+        return TemplateResponse(request, 'admin/stats.html', {
+            "course_stats": course_stats
+        })
+
+
+admin_site = MyCourseAdminSite(name='iCourse')
 
 
 class CourseForm(forms.ModelForm):
@@ -31,9 +51,9 @@ class MyCourseAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Course, MyCourseAdmin)
-admin.site.register(Lesson)
-admin.site.register(User)
-admin.site.register(Tag)
-admin.site.register(Comment)
+admin_site.register(Category)
+admin_site.register(Course, MyCourseAdmin)
+admin_site.register(Lesson)
+admin_site.register(User)
+admin_site.register(Tag)
+admin_site.register(Comment)
